99a29c816678e8b77fbc1c74e08b8538ec38aba1
ZholonkoS <41541490+SerhiyZholonko@users.noreply.github.com>
2026-03-10 22:14:12 +0200

    first init

---
+++ PlanTasks/PlanTasks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PlanTasks
+//
+//  Created by apple on 10.03.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PlanTasks/PlanTasks/PlanTasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanTasksApp.swift
+//  PlanTasks
+//
+//  Created by apple on 10.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct PlanTasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
